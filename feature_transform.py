refactor; add goosenet; fixed the bug with sum_tree (precision); add an agent with rules; add goose_experience.py to generate experience;

feature_transform.py
@@ -1,6 +1,10 @@
-from kaggle_environments.envs.hungry_geese.hungry_geese import Action, Configuration, Observation,row_col
-from feature_proc import get_square_features, get_action_rotated_north_head, get_features
-from goose_tools import get_eps, get_distance
+from kaggle_environments.envs.hungry_geese.hungry_geese import Action, Configuration, Observation, row_col
+from goose_tools import get_eps_based_on_step, get_distance
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch
 
 
 class AbstractFeatureTransform:
@@ -32,7 +36,7 @@ class SimpleFeatureTransform(AbstractFeatureTransform):
 
     @staticmethod
     def get_reward(observation: Observation, prev_observation: Observation, env_reward):
-        return get_reward(observation, prev_observation, SimpleFeatureTransform.configuration)
+        return get_simple_reward(observation, prev_observation, SimpleFeatureTransform.configuration)
 
     @staticmethod
     def get_state(observation: Observation, prev_observation: Observation):
@@ -45,38 +49,18 @@ class SimpleFeatureTransform(AbstractFeatureTransform):
         return get_features(observation, prev_heads, SimpleFeatureTransform.configuration)
 
 
-class SquareWorld7x7FeatureTransform(AbstractFeatureTransform):
-    FEATURES_CHANNELS_IN = 12
-    FEATURES_HEIGHT = 7
-    FEATURES_WIDTH = 7
-    configuration = Configuration({"rows": 7, "columns": 7, 'hunger_rate': 40, 'episodeSteps': 200})
+def get_simple_reward(observation: Observation, prev_obs: Observation, configuration: Configuration):
+    index = observation.index
+    if len(observation.geese[index]) > len(prev_obs.geese[index]):
+        return len(observation.geese[index])
 
-    @staticmethod
-    def get_reward(observation: Observation, prev_observation: Observation, env_reward):
-        return get_reward(prev_observation, observation, SquareWorld7x7FeatureTransform.configuration)
+    if 1 < len(observation.geese[index]) < len(prev_obs.geese[index]):
+        return -1
 
-    @staticmethod
-    def get_state(observation: Observation, prev_observation: Observation):
-        prev_heads = get_heads(prev_observation, len(observation.geese))
-        return get_square_features(observation, prev_heads, SquareWorld7x7FeatureTransform.configuration)
+    if len(observation.geese[index]) == 0:
+        return observation.step - configuration.episode_steps
 
-    @staticmethod
-    def adjust_action(observation: Observation, prev_observation: Observation, action: Action):
-        prev_heads = get_heads(prev_observation, len(observation.geese))
-        return get_action_rotated_north_head(observation,
-                                             prev_heads,
-                                             action,
-                                             SquareWorld7x7FeatureTransform.configuration.rows,
-                                             SquareWorld7x7FeatureTransform.configuration.columns)
-
-
-def get_heads(observation, n_geese):
-    prev_heads = [-1] * n_geese
-    if observation is not None:
-        for i in range(n_geese):
-            prev_heads[i] = observation.geese[i][0] if len(observation.geese[i]) != 0 else 1
-
-    return prev_heads
+    return 0
 
 
 def get_reward(observation: Observation, prev_obs: Observation, configuration: Configuration):
@@ -86,7 +70,7 @@ def get_reward(observation: Observation, prev_obs: Observation, configuration: C
     if len(observation.geese[index]) > len(prev_obs.geese[index]):     # ate sth
         reward = len(observation.geese[index]) * 1.5
     elif len(observation.geese[index]) == 0 and len(prev_obs.geese[index]) >= 2:  # died like a hero
-        reward = -1 * get_eps(10, 1, observation.step, configuration.episode_steps)
+        reward = -1 * get_eps_based_on_step(10, 1, observation.step, configuration.episode_steps)
     elif len(observation.geese[index]) == 0 and len(prev_obs.geese[index]) == 1:  # died like a rat
          reward = -20
 
@@ -105,6 +89,7 @@ def get_reward(observation: Observation, prev_obs: Observation, configuration: C
     if len(observation.geese[index]) == 0:
         # check whether he has committed suicide
         head_y, head_x = row_col(prev_obs.geese[index][0], configuration.columns)
+        died_alone = True
         for i in range(len(prev_obs.geese)):
             if i == index or len(prev_obs.geese[i]) == 0:
                 continue
@@ -114,7 +99,70 @@ def get_reward(observation: Observation, prev_obs: Observation, configuration: C
             dist_y = get_distance(enemy_y, head_y, configuration.rows)
             if dist_y == 0 and dist_x == 1 or \
                dist_y == 1 and dist_x == 0:
-                reward -= 50
+                died_alone = False
                 break
+        if died_alone:
+            reward -= 60
     # return torch.tensor([[reward]], device=device, dtype=torch.long)
     return reward
+
+
+def ids2locations(ids, prev_head, step, rows, columns):
+    state = np.zeros((4, rows * columns))
+    if len(ids) == 0:
+        return state
+    state[0, ids[0]] = 1  # goose head
+    if len(ids) > 1:
+        state[1, ids[1:-1]] = 1  # goose body
+        state[2, ids[-1]] = 1  # goose tail
+    if step != 0:
+        state[3, prev_head] = 1  # goose head one step before
+    return state
+
+
+def get_features(observation: Observation, prev_heads, configuration: Configuration):
+    rows, columns = configuration.rows, configuration.columns
+    geese = observation.geese
+    index = observation.index
+    step = observation.step
+    # convert indices to locations
+    locations = np.zeros((len(geese), 4, rows * columns))
+    for i, g in enumerate(geese):
+        locations[i] = ids2locations(g, prev_heads[i], step, rows, columns)
+    if index != 0:  # swap rows for player locations to be in first channel
+        locations[[0, index]] = locations[[index, 0]]
+    # put locations into features
+    features = np.zeros((12, rows * columns))
+    for k in range(4):
+        # склеить все части тела для каждого гуся (кроме предыдущей головы)
+        features[k] = np.sum(locations[k][:3], 0)
+    # склеить соответствующие состояния всех гусей (либо головы всех, либо тело, либо хвосты, либо пред. головы)
+    for k in range(4):
+        features[k + 4] = np.sum(locations[:, k], 0)
+
+    # только пред. голова наешего героя
+    features[-5, :] = locations[index][3]
+
+    features[-4, observation.food] = 1  # food channel
+    features[-3, :] = (step % configuration.hunger_rate) / configuration.hunger_rate  # hunger danger channel
+    features[-2, :] = step / configuration.episode_steps  # timesteps channel
+    features[-1, :] = float((step + 1) % configuration.hunger_rate == 0)  # hunger milestone indicator
+    features = torch.Tensor(features).reshape(-1, rows, columns)
+    # roll
+    if len(geese[index]) > 0:
+        head_row, head_col = row_col(geese[index][0], columns)
+    else:
+        head_row, head_col = row_col(prev_heads[index], columns)
+
+    features = torch.roll(features, ((rows // 2) - head_row, (columns // 2) - head_col), dims=(-2, -1))
+    return features
+
+
+def plot_features(features, title="Figure"):
+    fig, axs = plt.subplots(3, 4, figsize=(20, 10))
+    for i in range(3):
+        for j in range(4):
+            sns.heatmap(features[i * 4 + j], ax=axs[i, j], cmap='Blues',
+                        vmin=0, vmax=1, linewidth=2, linecolor='black', cbar=False)
+    plt.title(title)
+    plt.show()

goose_tools.py
@@ -1,9 +1,9 @@
+from kaggle_environments import make
 import numpy as np
 import math
-from kaggle_environments.envs.hungry_geese.hungry_geese import  Observation
 
 
-def get_eps(start_value, end_value, step, episodes):
+def get_eps_based_on_step(start_value, end_value, step, episodes):
     return end_value + (start_value - end_value) * (episodes - step) / episodes
 
 
@@ -11,12 +11,12 @@ def get_distance(x1, x2, max_coord):
     return np.min([int(math.fabs(x1 - x2)), int(max_coord - math.fabs(x1 - x2))])
 
 
-def observation_copy(observation: Observation):
-    return Observation(
-            {
-                'index': observation.index,
-                'geese': [x.copy() for x in observation.geese],
-                'step': observation.step,
-                'food': observation.food.copy(),
-                'remainingOverageTime': observation.remaining_overage_time
-            })
+def record_game(agent, enemies, res_path="game.html"):
+    env = make("hungry_geese", debug=True)
+    env.reset()
+
+    env.run([agent] + enemies)
+    out = env.render(mode="html", width=500, height=400)
+
+    with open(res_path, "w") as f:
+        f.write(out)

goose_war.py
@@ -1,12 +1,12 @@
 import torch
-from kaggle_environments import make, evaluate
+from kaggle_environments import evaluate
 import numpy as np
 import seaborn as sns
-from feature_transform import SimpleFeatureTransform, SquareWorld7x7FeatureTransform
+from feature_transform import SimpleFeatureTransform
 
 from goose_agents import RLAgent, GreedyAgent, AgentFactory
 import matplotlib.pyplot as plt
-from qestimator import QEstimator
+from qestimator import OneLayerNetQEstimator, GooseNet, AlexNetQEstimator
 
 
 class GooseWar:
@@ -45,7 +45,7 @@ class GooseWar:
 
 if __name__ == "__main__":
     device = 'cpu'
-    factory = AgentFactory(None, GreedyAgent, SimpleFeatureTransform, device)
+    factory = AgentFactory(None, GreedyAgent, SimpleFeatureTransform)
 
     name_to_factory = {"greedy1": factory}
 
@@ -65,37 +65,54 @@ if __name__ == "__main__":
     # name_to_factory["35k_7x11_ks_432_24_48"] = agent_factory
     #######
     #
-    # #####
-    net_path = "./results/35k_7x11_ks_232_24_48_simple_DQN.net"
-    net = QEstimator(c=12,
-                     h=7,
-                     w=11,
-                     kernel_sizes=[(2, 2), (3, 3), (2, 2)],
-                     layer_channels=[32, 48, 48],
-                     outputs=4,
-                     device=device).to(device)
+
+    #####
+
+    net_path = "./results/40000_OneLayerNetQEstimator_ks33_33_00_ch32_64_256_c12_h7_w11_DQN.net"
+    net = OneLayerNetQEstimator()
 
     net.load_state_dict(torch.load(net_path))
     net.eval()
-    agent_factory = AgentFactory(net, RLAgent, SimpleFeatureTransform, device)
-    name_to_factory["35k_7x11_ks_232_24_48"] = agent_factory
+    agent_factory = AgentFactory(net, RLAgent, SimpleFeatureTransform)
+    name_to_factory["40000_OneLayerNetQEstimator"] = agent_factory
     #######
 
-    # #####
-    # net_path = "./results/30k_7x11_ks_534_24_48_simple_DQN.net"
-    # net = QEstimator(c=12,
-    #                  h=7,
-    #                  w=11,
-    #                  kernel_sizes=[(5, 5), (3, 3), (4, 4)],
-    #                  layer_channels=[24, 48, 48],
-    #                  outputs=4,
-    #                  device=device).to(device)
+    #####
+
+    net_path = "./results/5000_OneLayerNetQEstimator_ks33_33_00_ch32_64_256_c12_h7_w11_DQN.net"
+    net = OneLayerNetQEstimator()
+
+    net.load_state_dict(torch.load(net_path))
+    net.eval()
+    agent_factory = AgentFactory(net, RLAgent, SimpleFeatureTransform)
+    name_to_factory["5000_OneLayerNetQEstimator"] = agent_factory
+    #######
+    #####
+
+    # net_path = "./results/40000_AlexNetQEstimator_ks33_33_00_ch32_64_256_c12_h7_w11_DQN.net"
+    # net = AlexNetQEstimator(c=12,
+    #                             h=7,
+    #                             w=11,
+    #                             kernel_sizes=[(3, 3), (3, 3), (4, 4)],
+    #                             layer_channels=[32, 64, 48],
+    #                             outputs=4,
+    #                             device=device).to(device)
     #
     # net.load_state_dict(torch.load(net_path))
     # net.eval()
     # agent_factory = AgentFactory(net, RLAgent, SimpleFeatureTransform, device)
-    # name_to_factory["30k_7x11_ks_534_24_48"] = agent_factory
-    # #######
+    # name_to_factory["40000_AlexNetQEstimator"] = agent_factory
+
+    #####
+
+    net_path = "./results/20000_YarEstimator_c12_h7_w11_DQN.net"
+    net = GooseNet()
+
+    net.load_state_dict(torch.load(net_path))
+    net.eval()
+    agent_factory = AgentFactory(net, RLAgent, SimpleFeatureTransform)
+    name_to_factory["20000_YarEstimator"] = agent_factory
+    #######
     # #####
     # net_path = "./results/20k_7x11_ks232_l64l32l16_simple.net"
     # net = QEstimator(c=12,

qestimator.py
@@ -3,152 +3,106 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class QEstimator(nn.Module):
-
-    def __init__(self, c, h, w, kernel_sizes, layer_channels, outputs, device):
-        super(QEstimator, self).__init__()
-
-        self.device = device
-        self.kernel_sizes = kernel_sizes
-        self.channels = layer_channels
-        strides = [(1, 1), (1, 1), (1, 1)]
-        dilations = [(1, 1), (1, 1), (1, 1)]
-        paddings = [(1, 1), (1, 1), (1, 1)]
-        padding_mode = 'circular'
-
-        self.conv1 = nn.Conv2d(c, self.channels[0],
-                               kernel_size=self.kernel_sizes[0],
-                               stride=strides[0],
-                               dilation=dilations[0],
-                               padding=paddings[0],
-                               padding_mode=padding_mode)
-
-        self.bn1 = nn.BatchNorm2d(self.channels[0])
-        self.conv2 = nn.Conv2d(self.channels[0], self.channels[1],
-                               kernel_size=self.kernel_sizes[1],
-                               stride=strides[1],
-                               dilation=dilations[1],
-                               padding=paddings[1],
-                               padding_mode=padding_mode)
-        self.bn2 = nn.BatchNorm2d(self.channels[1])
-        self.conv3 = nn.Conv2d(self.channels[1], self.channels[2],
-                               kernel_size=self.kernel_sizes[2],
-                               stride=strides[1],
-                               dilation=dilations[1],
-                               padding=paddings[1],
-                               padding_mode=padding_mode)
-
-        self.bn3 = nn.BatchNorm2d(self.channels[2])
-
-        # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, padding, dilation, kernel_size, stride):
-            return (size + 2 * padding - dilation * (kernel_size - 1) - 1) // stride + 1
-
-        conv_h = h
-        conv_w = w
-        for i in range(3):
-            conv_h = conv2d_size_out(conv_h, paddings[i][0], dilations[i][0], self.kernel_sizes[i][0], strides[i][0])
-            conv_w = conv2d_size_out(conv_w, paddings[i][1], dilations[i][1], self.kernel_sizes[i][1], strides[i][1])
-
-        linear_input_size = conv_w * conv_h * self.channels[2]
-        self.head = nn.Linear(linear_input_size, outputs)
-
-    # Called with either one element to determine next action, or a batch
-
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        x = x.to(self.device)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
-
-    def __str__(self):
-        kernel_str = "ks{0}{1}_{2}{3}_{4}{5}".format(self.kernel_sizes[0][0],
-                                                     self.kernel_sizes[0][1],
-                                                     self.kernel_sizes[1][0],
-                                                     self.kernel_sizes[1][1],
-                                                     self.kernel_sizes[2][0],
-                                                     self.kernel_sizes[2][1])
-        layer_str = "ch{0}_{1}_{2}".format(self.channels[0],
-                                           self.channels[1],
-                                           self.channels[2])
-
-        return "{0}_{1}_{2}".format(QEstimator.__name__, kernel_str, layer_str)
-
-
-class QEstimatorFactory:
-    def __init__(self, estimator_class, channel_in, height, width, kernel_sizes, layer_channels, device):
-        self.abstract_network_class = estimator_class
-        self.channels_in = channel_in
-        self.H = height
-        self.W = width
-        self.outputs = 4
-        self.device = device
-        self.kernel_sizes = kernel_sizes
-        self.layer_channels = layer_channels
-
-    def create(self):
-        return self.abstract_network_class(c=self.channels_in,
-                                           h=self.H,
-                                           w=self.W,
-                                           kernel_sizes=self.kernel_sizes,
-                                           layer_channels=self.layer_channels,
-                                           outputs=self.outputs,
-                                           device=self.device).to(self.device)
-
-
 class AlexNetQEstimator(nn.Module):
 
-    def __init__(self, c, h, w, kernel_sizes, layer_channels, outputs, device) -> None:
+    def __init__(self) -> None:
         super(AlexNetQEstimator, self).__init__()
-        self.device = device
-        self.kernel_sizes = [(3, 3), (3, 3), (0, 0)]
-        self.channels = [32, 64, 256]
         self.features = nn.Sequential(
-            nn.Conv2d(12, 32, kernel_size=3, stride=1, padding=2),      # (7, 11) -> (9, 13)
+            nn.Conv2d(12, 32, kernel_size=(3, 4), stride=1, padding=(1, 0), padding_mode='circular'),# (7, 11) -> (7, 8)
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # (9,13) -> (4, 6)
-            nn.Conv2d(32, 64, kernel_size=3, padding=2),    # (4, 6) -> (5, 7)
+            nn.Conv2d(32, 64, kernel_size=3, padding=0),    # (7, 8) -> (5, 6)
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # (5, 7) -> (2, 3)
-            # nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            # nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=3, padding=0),   # (5, 6) -> (3, 4)
+            nn.ReLU(inplace=True),
             # nn.Conv2d(128, 64, kernel_size=3, padding=1),
             # nn.ReLU(inplace=True),
             # nn.Conv2d(64, 64, kernel_size=3, padding=1),
             # nn.ReLU(inplace=True),
             # nn.MaxPool2d(kernel_size=2, stride=2),
         )
-        self.avgpool = nn.AdaptiveAvgPool2d((2, 2))
+        # self.avgpool = nn.AdaptiveAvgPool2d((2, 2))
         self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(64 * 2 * 2, 256),
+            # nn.Dropout(),
+            nn.Linear(128 * 3 * 4, 1024),
             nn.ReLU(inplace=True),
             # nn.Dropout(),
             # nn.Linear(512, 512),
             # nn.ReLU(inplace=True),
-            nn.Linear(256, outputs),
+            nn.Linear(1024, 4),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x.to(self.device)
         x = self.features(x)
-        x = self.avgpool(x)
+        # x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
 
     def __str__(self):
-        kernel_str = "ks{0}{1}_{2}{3}_{4}{5}".format(self.kernel_sizes[0][0],
-                                                     self.kernel_sizes[0][1],
-                                                     self.kernel_sizes[1][0],
-                                                     self.kernel_sizes[1][1],
-                                                     self.kernel_sizes[2][0],
-                                                     self.kernel_sizes[2][1])
-        layer_str = "ch{0}_{1}_{2}".format(self.channels[0],
-                                           self.channels[1],
-                                           self.channels[2])
+        return "{0}".format(AlexNetQEstimator.__name__)
 
-        return "{0}_{1}_{2}".format(AlexNetQEstimator.__name__, kernel_str, layer_str)
+
+class OneLayerNetQEstimator(nn.Module):
+
+    def __init__(self) -> None:
+        super(OneLayerNetQEstimator, self).__init__()
+        self.features = nn.Sequential(
+            # nn.Conv2d(12, 12, kernel_size=1, stride=1, padding=0),      # (7, 11) -> (7, 11)
+            # nn.ReLU(inplace=True),
+            nn.Conv2d(12, 64, kernel_size=(3, 4), stride=(3, 4), padding=(1, 1), padding_mode='circular'),  # (7, 11) -> (3, 3)
+            nn.ReLU(inplace=True),
+            # nn.ReLU(inplace=True),
+            # nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        # self.avgpool = nn.AdaptiveAvgPool2d((2, 2))
+        self.classifier = nn.Sequential(
+            nn.Linear(64 * 3 * 3, 4),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        # x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+    def __str__(self):
+        return "{0}".format(OneLayerNetQEstimator.__name__)
+
+
+class GooseNet(nn.Module):
+    def __init__(self):
+        n_channels = 12
+        super(GooseNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=12, out_channels=n_channels,
+                               kernel_size=3, padding=(1, 1), padding_mode='circular')
+        self.conv2 = nn.Conv2d(in_channels=n_channels, out_channels=2 * n_channels,
+                               kernel_size=3, padding=(1, 1), padding_mode='circular')
+        self.conv3 = nn.Conv2d(in_channels=2 * n_channels, out_channels=4 * n_channels,
+                               kernel_size=3, padding=(1, 1), padding_mode='circular')
+        self.final_conv = nn.Conv2d(in_channels=4 * n_channels, out_channels=1,
+                                    kernel_size=5, padding=0)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        north = x[:, :, 0:5, 3:8]
+        east = torch.rot90(x[:, :, 1:6, 4:9], 1, [2, 3])
+        south = torch.rot90(x[:, :, 2:7, 3:8], 2, [2, 3])
+        west = torch.rot90(x[:, :, 1:6, 2:7], 3, [2, 3])
+
+        # т.к. выходной вектор сети имеет форму [batch_size,1,1,4] - число батчей, число каналов, высота, ширина
+        # (последний слой - это сверточная сеть, тут не очень очевидно с выходом, может добавить линейный слой?)
+        # убираем лишние оси
+        north = self.final_conv(north).squeeze(1).squeeze(2)
+        east = self.final_conv(east).squeeze(1).squeeze(2)
+        south = self.final_conv(south).squeeze(1).squeeze(2)
+        west = self.final_conv(west).squeeze(1).squeeze(2)
+        # Склеиваем все результаты в один батч
+        return torch.cat([north, east, south, west], 1)
+
+    def __str__(self):
+       return "YarEstimator"

replay_memory.py
@@ -1,102 +1,22 @@
-from collections import namedtuple
 import random
-import numpy
-import torch
 import numpy as np
 import math
-from goose_tools import get_eps
-
-
-# SumTree
-# a binary tree data structure where the parent’s value is the sum of its children
-class SumTree:
-    MIN_POSSIBLE_PRIORITY = 1e-5
-    write = 0
-    min_priority = 0xffffffff
-
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.tree = numpy.zeros(2 * capacity - 1)
-        self.data = numpy.zeros(capacity, dtype=object)
-        self.n_entries = 0
-        self.pending_idx = set()
-
-    # update to the root node
-    def _propagate(self, idx, change):
-        parent = (idx - 1) // 2
-        self.tree[parent] += change
-        if parent != 0:
-            self._propagate(parent, change)
-
-    # find sample on leaf node
-    def _retrieve(self, idx, s):
-        left = 2 * idx + 1
-        right = left + 1
-
-        if left >= len(self.tree):
-            return idx
-
-        if s <= self.tree[left]:
-            return self._retrieve(left, s)
-        else:
-            return self._retrieve(right, s - self.tree[left])
-
-    def total(self):
-        return self.tree[0]
-
-    # store priority and sample
-    def add(self, p, data):
-        if p == 0:
-            p = self.MIN_POSSIBLE_PRIORITY
-
-        idx = self.write + self.capacity - 1
-        self.pending_idx.add(idx)
-
-        self.data[self.write] = data
-        self.update(idx, p)
-
-        self.write += 1
-        if self.write >= self.capacity:
-            self.write = 0
-
-        if self.n_entries < self.capacity:
-            self.n_entries += 1
-
-        if self.min_priority > p:
-            self.min_priority = p
-
-    # update priority
-    def update(self, idx, p):
-        if idx not in self.pending_idx:
-            return
-        self.pending_idx.remove(idx)
-        change = p - self.tree[idx]
-        self.tree[idx] = p
-        self._propagate(idx, change)
-
-    # get priority and sample
-    def get(self, s):
-        idx = self._retrieve(0, s)
-        dataIdx = idx - self.capacity + 1
-        self.pending_idx.add(idx)
-        return (idx, self.data[dataIdx], self.tree[idx])
-
-
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
+from sum_tree import SumTree
 
 
 class ReplayMemory(object):
+    MAXIMAL_PRIORITY = 100
 
     def __init__(self, capacity):
-        # self.memory = deque([], maxlen=capacity)
-        # self.memory = queue.PriorityQueue(capacity)
         self.sum_tree = SumTree(capacity)
         self.capacity = capacity
+        self.n_data_with_max_priority = 0
 
-    def push(self, priority, *args):
+    def push(self, priority, data):
         """Save a transition"""
-        self.sum_tree.add(priority, Transition(*args))
+        self.sum_tree.add(priority, data)
+        if priority == ReplayMemory.MAXIMAL_PRIORITY:
+            self.n_data_with_max_priority += 1
 
     def get(self, batch_size):
         if self.sum_tree.n_entries == 0:
@@ -109,39 +29,20 @@ class ReplayMemory(object):
 
         return sample
 
-    def update(self, idx, p):
-        self.sum_tree.update(idx, p)
-
     def __len__(self):
         return self.sum_tree.n_entries
 
+    def update_priorities(self, idx_list, priorities):
+        for i in range(len(idx_list)):
+            if self.sum_tree.tree[idx_list[i]] == ReplayMemory.MAXIMAL_PRIORITY:
+                self.n_data_with_max_priority = np.max(self.n_data_with_max_priority - 1, 0)
+            self._update(idx_list[i], priorities[i])
 
-def get_max_annealing_parameter(replay_memory, step, max_step):
-    return annealing_parameter(replay_memory, replay_memory.sum_tree.min_priority,
-                               step, max_step)
+    def is_there_new_experience(self):
+        return self.n_data_with_max_priority > 0
 
-
-def annealing_parameter(replay_memory, priority, step, max_step):
-    N = len(replay_memory)
-    beta = get_eps(0.2, 1, step, max_step)
-    prob = priority / replay_memory.sum_tree.total()
-
-    omega = math.pow(1 / N / prob, beta)
-
-    return omega
-
-
-def update_priorities(idx_list, priorities, memory):
-    for i in range(len(idx_list)):
-        memory.update(idx_list[i], priorities[i])
-
-
-def calculate_priority(reward, prev_state, state, action, target, policy, gamma):
-    # R_j + gamma_j * Q_target(S_j, argmax_a(S_j, a)) - Q(S_{j - 1}, A_{j - 1})
-    with torch.no_grad():
-        q1 = policy(prev_state.unsqueeze(0)).squeeze(0)[action]
-        q2 = reward + gamma * np.argmax(target(state.unsqueeze(0)).squeeze(0)) if state is not None else reward
-        return np.abs(q2 - q1)
+    def _update(self, idx, p):
+        self.sum_tree.update(idx, p)
 
 
 # В оригинальной статье Prioritized Experience Replay работают с одним приоритетом и для него одного вычисляется
@@ -149,12 +50,22 @@ def calculate_priority(reward, prev_state, state, action, target, policy, gamma)
 # Здесь же попытка сделать это для батчей, просто берется среднее всех коэффициентов
 # так же подсчет максимального коэффициента отжига считается на основании минимального
 # встретившегося в памяти приоритета не на данный момент, а в течение всей работы с этой коллекцией
-def get_priority_weight(priorities, replay_memory, step, max_step):
-    max_annealing_param = get_max_annealing_parameter(replay_memory, step, max_step)
+def get_priority_weight(priorities, replay_memory, beta):
+    max_annealing_param = get_max_annealing_parameter(replay_memory, beta)
     map_func = lambda x: \
         math.pow(max_annealing_param, -1) * annealing_parameter(replay_memory,
                                                                 x,
-                                                                step,
-                                                                max_step)
+                                                                beta)
     weights_c = list(map(map_func, priorities))
     return np.average(weights_c)
+
+
+def get_max_annealing_parameter(replay_memory, beta):
+    return annealing_parameter(replay_memory, replay_memory.sum_tree.min_priority, beta)
+
+
+def annealing_parameter(replay_memory, priority, beta):
+    N = len(replay_memory)
+    prob = priority / replay_memory.sum_tree.total()
+    omega = math.pow(1 / N / prob, beta)
+    return omega