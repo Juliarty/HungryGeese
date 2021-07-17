4a4d081a7f778bb74f8cfaaac0dd472fc5b217d4
Juliarty <sirrzk.09@yandex.ru>
2021-07-18 02:50:43 +0300

    extended goose_tools;

--- goose_tools.py
+++ goose_tools.py
@@ -1,7 +1,9 @@
 from kaggle_environments import make
-from kaggle_environments.envs.hungry_geese.hungry_geese import Observation, Configuration, Action, row_col
+from kaggle_environments.envs.hungry_geese.hungry_geese import Observation, Configuration, Action, row_col, translate
 import numpy as np
 import math
+
+configuration = Configuration({"rows": 7, "columns": 11, 'hunger_rate': 40, 'episodeSteps': 200})
 
 
 def get_eps_based_on_step(start_value, end_value, step, episodes):
@@ -40,3 +42,26 @@ def is_near_to_enemy(observation: Observation):
             if dist_y == 1 or dist_x == 1:
                 return True
     return False
+
+
+def get_prev_actions(observation: Observation, prev_obs: Observation):
+    if prev_obs is None:
+        return None
+    result = []
+    for i in range(len(observation.geese)):
+        if len(observation.geese[i]) == 0 or len(prev_obs.geese[i]) == 0:
+            result.append(Action(1))
+            continue
+        head = observation.geese[i][0]
+
+        prev_head = prev_obs.geese[i][0]
+
+        for action in Action:
+            if head == translate(prev_head, action, configuration.columns, configuration.rows):
+                result.append(action)
+
+    try:
+        assert(len(result) == len(observation.geese))
+    except:
+        pass
+    return result
